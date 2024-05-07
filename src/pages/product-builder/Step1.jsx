b3fdfceb55b915defaea6b95d1f5f56408b3bad9
rashidpkn <muhammed916rashid@gmail.com>
2024-05-07 16:56:41 +0530

    Somthing isa dded

--- src/pages/product-builder/Step1.jsx
+++ src/pages/product-builder/Step1.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setFabric } from "../../redux/slice/fabric";
 import { fabrics } from "./fabrics";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
  const Step1 = () => {
     const dispatch = useDispatch()
@@ -9,6 +9,12 @@ import { useState } from "react";
 
     const [types, setTypes] = useState('pattern') // plain
     
+    const totalPage  = Math.ceil(fabrics.filter(e=>e.texture === types).length/24)
+
+   const [pageNo, setPageNo] = useState(1)
+
+    
+
       return (
         <>
         <div className="flex justify-center items-center">
@@ -23,13 +29,19 @@ import { useState } from "react";
           <div className="grid grid-cols-8 gap-2 mt-8">
 
 
-            {fabrics.map((e) =>e.texture === types && (
+            {fabrics.filter(e=>e.texture === types).slice((pageNo - 1) * 24, pageNo * 24).map((e) => (
               <div key={e.id} className={`h-12 w-12 rounded-full  p-[3px] border-0 ${e.fabric === type && 'border-4 border-[#e1999f]'}  hover:border-4 duration-200 cursor-pointer`}>
                 <button onClick={()=>dispatch(setFabric({key:'type',value:e.fabric}))} title={e.fabric} className={`w-full h-full rounded-full overflow-hidden`} >
                   <img className="h-full w-full" src={e.fabricPath} alt={e.fabric}  />
                 </button>
               </div>
             ))}
+
+            <div className="col-span-full flex justify-center gap-3">
+              
+                  {Array.from({length: totalPage}, (_, i) => i + 1).map(e=><button onClick={()=>setPageNo(e)} key={e} className={`rounded-full bg-[#e1999f]/30 text-white h-5 w-5 flex justify-center items-center ${pageNo === e ? 'border-[#e1999f]' : 'border-transparent'} border-[2px]    p-3 `}>{e}</button>)}
+            </div>
+
           </div>
           <div className="p-3 border rounded mt-6">
             <p className="text-xs font-medium text-[#212122]">
